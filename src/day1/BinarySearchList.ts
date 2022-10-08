5b6e4e84cd8058a52e9f1ac1e4443b5576f305a3
John <john.bow7720@gmail.com>
2022-10-08 01:32:36 -0600

    implement linear and binary search, kata day 1

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,27 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+// Binary search works on an ordered input. Because we know it is ordered we can jump straight
+// to the half way point and determine if we found our value, need to check the lower values,
+// or need to check the higher values; always going to the middle of each direction we go.
+// This results in an O(logN) time complexity
 
+export default function bs_list(haystack: number[], needle: number): boolean {
+  // start by defining our hi and low
+  let low = 0;
+  let hi = haystack.length;
+  
+  // loop until hi and low are equal, meaning we've divided in half as much as we possibly could without finding the answer
+  while (low < hi) {
+    // compute our mid and value at the start of each iteration
+    let mid = Math.floor(low + (hi - low) / 2);
+    let value = haystack[mid];
+
+    if (value === needle) {
+      return true;
+    } else if (value > needle) {
+      hi = mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+  // never found out value
+  return false;
 }
--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,17 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
+// Linear Search is a simple "step through each element of the array looking for the value" algorithm
+// because in the worst case we step through every item of the array that means that this algorithm
+// is O(n) - linear - time complexity
 
+export default function linear_search(
+  haystack: number[],
+  needle: number,
+): boolean {
+  // check every value from the beginning to end, returning once it is found
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle) {
+      return true;
+    }
+  }
+  // never found our value
+  return false;
 }
