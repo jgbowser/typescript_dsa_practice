5415ee2edffe3cc39338159aa4bf6e3c59a402d1
John <john.bow7720@gmail.com>
2022-10-09 01:12:09 -0600

    implement a Queue using a linked list

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,45 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const newNode: Node<T> = {
+      value: item,
+    };
+
+    if (this.tail === undefined) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
 
-    enqueue(item: T): void {
-
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    this.length++;
+  }
+  deque(): T | undefined {
+    if (this.head !== undefined) {
+      const head = this.head;
+      this.head = head?.next;
+      this.length--;
+      if (this.length === 0) {
+        this.tail = undefined;
+      }
+      return head?.value;
+    }
+    return;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
