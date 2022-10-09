0df19dc00583ffc42a01bf5388c9472c7445151b
John <john.bow7720@gmail.com>
2022-10-09 00:04:56 -0600

    katas day 1, implement singly linked list class

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
-export default function bubble_sort(arr: number[]): void {
+// Sorting algorithm in which each iteration through the array results in the largest
+// number moving to the end. This means each time we iterate through, we can stop 1
+// index sooner, because we know things at the end are sorted.
+// results in a time complexity of O(N^2)
 
+export default function bubble_sort(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
 }
--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,156 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
 export default class SinglyLinkedList<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  prepend(item: T): void {
+    const newNode: Node<T> = {
+      value: item,
+    };
+
+    if (this.head === undefined) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.next = undefined;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
     }
 
-    prepend(item: T): void {
+    this.length++;
+  }
 
-}
-    insertAt(item: T, idx: number): void {
+  insertAt(item: T, idx: number): void {
+    if (idx - 1 > this.length) {
+      console.error("Provided index is out of bounds of the linked list");
+      return;
+    }
 
-}
-    append(item: T): void {
+    const newNode: Node<T> = {
+      value: item,
+    };
+    let currNode = this.head as Node<T>;
 
-}
-    remove(item: T): T | undefined {
+    for (let i = 0; i < idx; i++) {
+      if (currNode.next !== undefined) {
+        currNode = currNode.next;
+      }
+    }
 
-}
-    get(idx: number): T | undefined {
+    newNode.next = currNode.next;
+    currNode.next = newNode;
 
-}
-    removeAt(idx: number): T | undefined {
+    this.length++;
+  }
 
-}
+  append(item: T): void {
+    const newNode: Node<T> = {
+      value: item,
+    };
+
+    if (this.tail === undefined) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.next = undefined;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+
+  remove(item: T): T | undefined {
+    if (this.head !== undefined) {
+      let currNode: Node<T> | undefined = this.head;
+      let prevNode: Node<T> | undefined = undefined;
+      for (let i = 0; i < this.length; i++) {
+        if (currNode?.value === item) {
+          if (i === 0) {
+            const targetNode = currNode;
+            this.head = targetNode.next;
+            this.length--;
+            return targetNode.value;
+          } else if (i === this.length - 1) {
+            const targetNode = currNode;
+            (prevNode as Node<T>).next = undefined;
+            this.tail = prevNode;
+            this.length--;
+            return targetNode.value;
+          } else {
+            const targetNode = currNode;
+            (prevNode as Node<T>).next = currNode.next;
+            this.length--;
+            return targetNode.value;
+          }
+        }
+        prevNode = currNode;
+        currNode = currNode?.next;
+      }
+    }
+    return undefined;
+  }
+
+  get(idx: number): T | undefined {
+    if (idx - 1 > this.length) {
+      console.error(
+        `index: ${idx} is outside of the bounds of the linked list`,
+      );
+      return undefined;
+    }
+
+    if (this.head !== undefined) {
+      let currNode = this.head;
+      for (let i = 0; i < idx; i++) {
+        currNode = currNode.next as Node<T>;
+      }
+      return currNode.value;
+    }
+
+    return undefined;
+  }
+
+  removeAt(idx: number): T | undefined {
+    if (idx - 1 > this.length) {
+      console.error(
+        `index: ${idx} is outside of the bounds of the linked list`,
+      );
+      return undefined;
+    }
+
+    if (this.head !== undefined) {
+      if (idx === 0) {
+        const targetNode = this.head;
+        this.head = targetNode.next;
+        this.length--;
+        return targetNode.value;
+      }
+      let currNode = this.head;
+      let prevNode: Node<T> | undefined = undefined;
+
+      for (let i = 0; i < idx; i++) {
+        prevNode = currNode;
+        currNode = currNode.next as Node<T>;
+      }
+
+      if (prevNode !== undefined) {
+        prevNode.next = currNode.next;
+        if (prevNode.next === undefined) {
+          this.tail = prevNode;
+        }
+      }
+      this.length--;
+      return currNode.value;
+    }
+    return undefined;
+  }
 }
