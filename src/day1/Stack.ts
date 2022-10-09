1148ba15f854e1f2ec7cf1504f445ddf6f81453d
John <john.bow7720@gmail.com>
2022-10-09 11:15:06 -0600

    implement a Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,34 @@
+type Node<T> = {
+  value: T;
+  prev?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const newNode = { value: item } as Node<T>;
+    newNode.prev = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+  pop(): T | undefined {
+    if (!this.head) {
+      return;
     }
 
-    push(item: T): void {
-
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    const head = this.head;
+    this.head = head.prev;
+    this.length--;
+    return head.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
